80b566b3ae6f4e148212374a3fb1a928dc773965
izaacvictorsoares10-cell <izaacvictorsoares10@gmail.com>
2026-05-20 17:24:04 -0300

    Aprendi hj

---
+++ Exercicio07.py
@@ -0,0 +1,37 @@
+exercicio1 = (15>7) and (3==3)
+print(f"exercicio 1 e : {exercicio1}")
+
+
+exerc2 = (10<4) or (8!=5)
+print(f"exercicio2 e : {exerc2}")
+
+
+exerc3 = not(20>=20)
+print(f"exercicio3 e : {exerc3}")
+
+
+exerc4 = (5*2==10) and not(3>4)
+print(f"exercicio4 e: {exerc4}")
+
+
+exerc5 = (12/2==5) or (7<=7)
+print(f"exercicio5 e : {exerc5}")
+
+
+exerc6 = not(9!=9) and (4>1)
+print(f"exercicio6 e : {exerc6}")
+
+
+exerc7 = (100==100) and ((5>10) or (2<3))
+print(f"exercicio7 e : {exerc7}")
+
+
+exerc8 = not((18 <20) and (5!=5))
+print(f"exercicio8 e : {exerc8}")
+
+
+exerc9 = (0==1) or not((3>=2)and (4<=4))
+print(f"exercicio9 e : {exerc9}")
+
+exerc10 = ((10>5) and not(2==3) or(8<4))
+print(f"exercicio10 e : {exerc10}")
