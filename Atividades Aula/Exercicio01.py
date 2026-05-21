fd1c01799bfccb959d0af3769ecb8d87c65ac9c2
izaacvictorsoares10-cell <izaacvictorsoares10@gmail.com>
2026-05-21 17:35:32 -0300

    Exercicio01

---
+++ Atividades Aula/Exercicio01.py
@@ -0,0 +1,14 @@
+numerosLista = []
+
+for i in range(6):
+    numero = int(input(f"Digite o {i +1}° numero: "))
+    numerosLista.append(numero)
+
+numerosLista.sort()
+print(numerosLista)
+soma = sum(numerosLista)
+print(f"A soma dos numeros e = {soma}")
+print("O maior numero e =",(max(numerosLista)))
+print("O menor numero e =" ,(min(numerosLista)))
+
+
