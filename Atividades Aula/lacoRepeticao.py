f812731be67db7aff14b64d6543babe3cf8e1a18
izaacvictorsoares10-cell <izaacvictorsoares10@gmail.com>
2026-05-21 17:31:48 -0300

    aprendi hj

---
+++ Atividades Aula/lacoRepeticao.py
@@ -0,0 +1,10 @@
+alunos = ["Jaqueline", "Debora", "Izaac", "Escobarrrr", "Rafael", "Lua", "Arthur", "lindoso", "Wojcieskovsky", "Evilyn" ]
+
+alunos.sort()
+
+print("Quantos alunos tem na sala:  {len(alunos)}")
+
+print("--- Iniciando a chamada! ---")
+for i in alunos:
+    print(f"Alunos(a) {i} esta presente!")
+print("--- Fim da Chamada! ---")
---
+++ Atividades Aula/vetor.py
@@ -0,0 +1,27 @@
+import math
+
+temperatura = "frio"
+
+sala = ["monitor", "cadeiras" , 9, True, 18.5, "TV"]
+print(sala[1].upper())
+print(sala[-1].lower())
+
+
+
+"""
+if temperatura == "frio" :
+    sala.append("ar")
+    sala.append("Casaco")
+else:
+    sala.append("Caipirinha")
+    sala.append("29.90")
+
+"""
+
+print(sala[-1])
+
+notas = [4.5, 2.8, 7.3, 8.5, 9.0]
+media = sum(notas)/len(notas)
+
+print(f"A media e = {media}")
+print(math.pow(2, 0))
